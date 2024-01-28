544373c28f99ebd43a80c889b82a23ba7720e364
Larry Le <lle114@student.hbuhsd.edu>
2024-01-28 14:37:47 -0800

    Tab Navigation CSS
    Tab Bottom Bar icons change to white when focused and black when not focused. For example, clicking on the search icon (aka focusing it) will turn the icon white and all the unfocused icons will be black by default

--- scouting/Navigation.jsx
+++ scouting/Navigation.jsx
@@ -17,22 +17,22 @@ export default function Navigation() {
             headerShown: false,
             tabBarStyle: {
               backgroundColor: '#E1584B',
-              position: 'absolute',
-              bottom: 20,
-              width: '95%',
-              borderRadius: 15,
-
-              left: 10,
+              width: '100%',
             },
           }}
         >
             <Tab.Screen name="Home" component={Home}
               options={{
                 title: 'Home Page',
-                tabBarIcon: () => (
+                tabBarIcon: ({focused}) => (
+                  // The focused prop is active whenever the tab.screen component is active
 
                     // Entypo is a library of icons. Refer to this video https://www.youtube.com/watch?v=1d5Dtc1UL1c&ab_channel=CodeBig
-                    <Entypo name="home" size={30} color={'white'}  />
+                    <Entypo
+                      name="home"
+                      size={30}
+                      color={focused ? 'white' : 'black'}
+                      />
                   ),
                 tabBarShowLabel: false,
               }}
@@ -41,8 +41,8 @@ export default function Navigation() {
             <Tab.Screen name="Search" component={Search}
               options={{
                 title: 'Search Page',
-                tabBarIcon: () => (
-                    <AntDesign name="search1" size={30} color={'black'} />
+                tabBarIcon: ({focused}) => (
+                    <AntDesign name="search1" size={30} color={focused ? 'white' : 'black'} />
                   ),
                 tabBarShowLabel: false,
               }}
@@ -51,8 +51,8 @@ export default function Navigation() {
             <Tab.Screen name="Record" component={Record}
               options={{
                 title: 'Record Page',
-                tabBarIcon: () => (
-                    <AntDesign name="pluscircleo" size={30} color={'black'} />
+                tabBarIcon: ({focused}) => (
+                    <AntDesign name="pluscircleo" size={30} color={focused ? 'white' : 'black'} />
                   ),
                 tabBarShowLabel: false,
               }}
@@ -61,8 +61,8 @@ export default function Navigation() {
             <Tab.Screen name="Settings" component={Settings}
               options={{
                 title: 'Settings Page',
-                tabBarIcon: () => (
-                    <Ionicons name="settings-outline" size={30} color={'black'} />
+                tabBarIcon: ({focused}) => (
+                    <Ionicons name="settings-outline" size={30} color={focused ? 'white' : 'black'} />
                   ),
                 tabBarShowLabel: false,
               }}
@@ -71,65 +71,3 @@ export default function Navigation() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'white',
-      width: '100%',
-      height: '100%',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      flexDirection: 'column',
-    },
-    topPiece: {
-      width: '100%',
-      height: '8%',
-      backgroundColor: '#E1584B',
-    },
-    middlePiece: {
-      width: '90%',
-      maxHeight: '84%',
-      backgroundColor: 'white',
-
-      paddingTop: 20,
-      paddingBottom: 20,
-      gap: 30,
-      alignItems: 'center',
-    },
-    getStarted: {
-      minHeight: '15%',
-      width: '100%',
-      gap: 10,
-    },
-    viewScoutingData: {
-      width: '100%',
-      maxHeight: '84%',
-    },
-    bottomPiece: {
-      width: '100%',
-      height: '8%',
-      backgroundColor: '#E1584B',
-    },
-    header: {
-      color: '#616161',
-      fontSize: 17,
-    },
-    headerSmaller: {
-      color: '#616161',
-      fontSize: 14,
-    },
-    text: {
-      color: '#616161',
-      fontSize: 12,
-    },
-    text2: {
-      color: '#616161',
-      fontSize: 14.05,
-    },
-    image: {
-      width: 40,
-      height: 40,
-  },
-});
-
-
