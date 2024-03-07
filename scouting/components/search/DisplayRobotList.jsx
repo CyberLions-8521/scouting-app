cdedcceb6b819dd147bc9cc7799c216d88531d4c
Larry Le <lle114@student.hbuhsd.edu>
2024-03-07 00:44:45 -0800

    update filter method
    switched from robotID to teamNumber for all requests. This is more reliable when doing queries because when you delete a robot profile document and then create a new robot profile, it might repeat robotID's.

--- scouting/components/search/DisplayRobotList.jsx
+++ scouting/components/search/DisplayRobotList.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
 import axios from 'axios';
 import StatGlimpse from '../../components/home/StatGlimpse.jsx';
 
-export default function DisplayRobotList() {
+export default function DisplayRobotList({ navigation }) {
     const [genData, setGenData] = useState();
 
     useEffect(() => {
@@ -18,7 +18,7 @@ export default function DisplayRobotList() {
 
     return (
         <>
-            {genData?.filter((robot) => robot._id != 'robotID').map((robot) =>
+            {genData?.filter((robot) => robot._id !== 'robotID').map((robot) =>
                 <View key={robot.robotID}>
                     <StatGlimpse name={robot.profile.teamName} teamNumber={robot.profile.teamName} rank={5} winLossRatio={'3:5'} />
                 </View>
